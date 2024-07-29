351bc0df1fdfae15923b1ecef5db161554d5dcda
samdcole48 <samdcole48@gmail.com>
2024-07-29 13:00:18 -0500

    cleaned up homepage

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -2,9 +2,10 @@
   <v-container class="fill-height">
     <div class="align-centerfill-height mx-auto">
       <div style="font-size: 900%">
-        Hello, I'm <img height="150" src="@/assets/my-logo.png"></img>.
+        Hello, I'm <br>
+        <img height="150" src="@/assets/my-logo.png"></img>
       </div>
-      <div style="font-size: 400%">An Aspiring Software Developer.</div>
+      <div style="font-size: 400%">An Aspiring Software Developer</div>
     </div>
   </v-container>
 </template>
