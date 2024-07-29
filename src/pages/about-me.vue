4c2ce2da20f61d02b7f916e263dc637e51d29d74
samdcole48 <samdcole48@gmail.com>
2024-07-29 14:07:59 -0500

    start of about me page

--- src/pages/about-me.vue
+++ src/pages/about-me.vue
@@ -1,6 +1,16 @@
 <template>
   <v-container class="fill-height">
-    <div class="align-centerfill-height mx-auto">ABOUT ME</div>
+    <div class="align-centerfill-height mx-auto">
+      <v-row>
+        <v-col>
+          <img height="100" src="@/assets/my-logo.png"></img>
+          <div style="font-size: 200%">yap yap yap</div>
+        </v-col>
+        <v-col>
+          <img height="500" src="@/assets/headshot-no-bg.png"></img>
+        </v-col>
+      </v-row>
+    </div>
   </v-container>
 </template>
 
