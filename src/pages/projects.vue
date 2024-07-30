d1c5626e3b0d2774134c2d3a583211446c999f9b
samdcole48 <samdcole48@gmail.com>
2024-07-30 14:28:55 -0500

    added more projects

--- src/pages/projects.vue
+++ src/pages/projects.vue
@@ -76,7 +76,7 @@
       <two-even-containers class="py-16 my-16">
         <template #left>
           <title-text-and-links>
-            <template #title>MESS Waffles P.O.S. System</template>
+            <template #title>MESS Waffles POS System</template>
             <template #text>
               Developed a point of sales web app for MESS Waffles, a restraunt
               in College Station. Throughout this project I served as the Lead
@@ -119,6 +119,160 @@
             </v-carousel-item>
             <v-carousel-item>
               <v-img src="@/assets/mess/MESS_Orders.png" height="90%"></v-img>
+            </v-carousel-item>
+          </v-carousel>
+        </template>
+      </two-even-containers>
+
+      <two-even-containers class="py-16 my-16">
+        <template #left>
+          <title-text-and-links>
+            <template #title>64-bit x86 CPU</template>
+            <template #text>
+              Designed a fully functional 64-bit intel like x86 CPU from basic
+              logic gates in Logism. This was the final project for my computer
+              organization course, CSCE 312
+            </template>
+            <template #links>
+              <a
+                :style="{
+                  color: 'var(--v-theme-primary)',
+                  textDecoration: 'none',
+                }"
+                href="https://github.com/samdcole48/csce312-64-bit_x86_CPU"
+                target="_blank"
+                ><v-icon>mdi-github</v-icon>Git Repository</a
+              >
+            </template>
+          </title-text-and-links>
+        </template>
+        <template #right>
+          <v-carousel
+            :show-arrows="false"
+            delimiter-icon="mdi-square"
+            hide-delimiter-background
+          >
+            <v-carousel-item>
+              <div
+                class="d-flex justify-center align-center"
+                style="font-size: 150%"
+              >
+                CPU
+              </div>
+              <v-img src="@/assets/csce312/312_Main.png"></v-img>
+            </v-carousel-item>
+            <v-carousel-item>
+              <div
+                class="d-flex justify-center align-center"
+                style="font-size: 150%"
+              >
+                Fetch
+              </div>
+              <v-img src="@/assets/csce312/312_Fetch.png" height="90%"></v-img>
+            </v-carousel-item>
+            <v-carousel-item>
+              <div
+                class="d-flex justify-center align-center"
+                style="font-size: 150%"
+              >
+                Decode
+              </div>
+              <v-img src="@/assets/csce312/312_Decode.png" height="90%"></v-img>
+            </v-carousel-item>
+            <v-carousel-item>
+              <div
+                class="d-flex justify-center align-center"
+                style="font-size: 150%"
+              >
+                Execute
+              </div>
+              <v-img
+                src="@/assets/csce312/312_Execute.png"
+                height="90%"
+              ></v-img>
+            </v-carousel-item>
+            <v-carousel-item>
+              <div
+                class="d-flex justify-center align-center"
+                style="font-size: 150%"
+              >
+                Memory
+              </div>
+              <v-img src="@/assets/csce312/312_Memory.png" height="90%"></v-img>
+            </v-carousel-item>
+            <v-carousel-item>
+              <div
+                class="d-flex justify-center align-center"
+                style="font-size: 150%"
+              >
+                PC Update
+              </div>
+              <v-img
+                src="@/assets/csce312/312_PCupdate.png"
+                height="90%"
+              ></v-img>
+            </v-carousel-item>
+          </v-carousel>
+        </template>
+      </two-even-containers>
+
+      <two-even-containers class="py-16 my-16">
+        <template #left>
+          <title-text-and-links>
+            <template #title>Space Traveler</template>
+            <template #text>
+              HowdyHack 2022 project where my team and I created a game within
+              24 hours. The game is about an alien traveling through space
+              dodging obstacles. We even created our very own soundtrack! This
+              project was built using Python and the Pygame library.
+            </template>
+            <template #links>
+              <a
+                :style="{
+                  color: 'var(--v-theme-primary)',
+                  textDecoration: 'none',
+                }"
+                href="https://www.youtube.com/watch?v=S8zKgtNReRs&t"
+                target="_blank"
+                ><v-icon>mdi-youtube</v-icon>Demo Video</a
+              >
+              <br />
+              <a
+                :style="{
+                  color: 'var(--v-theme-primary)',
+                  textDecoration: 'none',
+                }"
+                href="https://github.com/spencertlogan/HH22SpaceTravelers"
+                target="_blank"
+                ><v-icon>mdi-github</v-icon>Git Repository</a
+              >
+            </template>
+          </title-text-and-links>
+        </template>
+        <template #right>
+          <v-carousel
+            :show-arrows="false"
+            delimiter-icon="mdi-square"
+            hide-delimiter-background
+          >
+            <v-carousel-item>
+              <v-img
+                src="@/assets/space_travelers/SpaceTravelers_Game.png"
+                height="90%"
+              ></v-img>
+            </v-carousel-item>
+            <v-carousel-item>
+              <v-img
+                src="@/assets/space_travelers/SpaceTravelers_Menu.png"
+                height="90%"
+              ></v-img>
+            </v-carousel-item>
+            <v-carousel-item>
+              <LiteYouTubeEmbed
+                id="S8zKgtNReRs"
+                title="Space Travelers Demo"
+                :aspectHeight="8.35"
+              />
             </v-carousel-item>
           </v-carousel>
         </template>
