1365ad3d4bf20e6ccf1c6f4b6b0b3a740943cdbb
samdcole48 <samdcole48@gmail.com>
2024-07-30 22:05:14 -0500

    removed boiler plate code and added consolelog for scrollBehavior

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,7 +6,6 @@
 
 // Composables
 import { createRouter, createWebHistory } from 'vue-router/auto'
-import HelloWorld from '@/components/HelloWorld.vue'
 import home from '@/pages/home.vue'
 import aboutMe from '@/pages/about-me.vue'
 import experience from '@/pages/experience.vue'
@@ -15,13 +14,13 @@ import projects from '@/pages/projects.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/hello', name: '/hello', component: HelloWorld },
     { path: '/about-me', name: '/about-me', component: aboutMe },
     { path: '/experience', name: '/experience', component: experience },
     { path: '/projects', name: '/projects', component: projects },
     { path: '/', name: '/', component: home },
   ],
   scrollBehavior(to, from, savedPosition) {
+    console.log('scrollBehavior called', { to, from, savedPosition });
     // always scroll to top
     return { top: 0 }
   },
